382cd26ab8b282b6fb73220281494290ccb3a6b5
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-01-20 21:24:25 -0500

    Time: 38 ms (62.70%) | Memory: 16.5 MB (56.96%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -14,7 +14,7 @@ class Solution:
         
         for i in range(2, len(nums)):
             # either robs the house or doesn't rob the house
-            # if robs , then robs both current and prev or prev as not adjacent
+            # if robs , then robs both current and prev of prev as not adjacent
             # if doesn't rob, then robs just the previous
             max_robbery.append(max(nums[i] + max_robbery[i - 2], max_robbery[i - 1]))
         
