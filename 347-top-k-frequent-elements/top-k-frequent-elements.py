4d774a0ecaf5e8e1ed63dcad91896f75179036de
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-03-11 23:53:52 -0400

    Time: 92 ms (43.58%) | Memory: 22.1 MB (18.10%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -5,7 +5,29 @@ class Solution:
         # pop top K elements
         # TC : O(nlogk)
 
+        # counts the freq of elemensts in an array - Counter()
+        # freq = Counter(nums)
+        # # https://pythontic.com/algorithms/heapq/nlargest
+        # return heapq.nlargest(k, freq.keys(), key = freq.get)
+
+        # Solution 2: Bucket sort
+        # sorts based on the count of the elements (max count when distinct = len(arr))
+        # traverse the count arr k times from the end
+
         freq = Counter(nums)
-        # https://pythontic.com/algorithms/heapq/nlargest
-        return heapq.nlargest(k, freq.keys(), key = freq.get)
+        bucket = [[] for i in range(len(nums) + 1)]
+
+        # build the bucket based on the count of each element in the array
+        for num, count in freq.items():
+            bucket[count].append(num)
+        
+        res = []
+        for i in range(len(bucket) - 1, 0, -1):
+            for most_freq_list in bucket[i]:
+                res.append(most_freq_list)
+                if len(res) == k:
+                    return res
+
+        
+
         
