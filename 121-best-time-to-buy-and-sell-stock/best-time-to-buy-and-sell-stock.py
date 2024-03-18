c688efe22eb11c555956b102c6d55a736a1b54fd
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-03-18 12:49:01 -0400

    Time: 691 ms (90.87%) | Memory: 27.4 MB (37.69%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,17 +1,30 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
 
-        left = 0 
-        right = left + 1
-        maxProfit = 0
+        # Sol 2: Kadane's algo (DP technique) - modified
+        buy = prices[0]
+        profit = 0
 
-        while right < len(prices):
-            if prices[left] < prices[right]:
-                maxProfit = max(maxProfit, prices[right] - prices[left])
-            else:
-                left = right # skip to the next lowest
-
-            right += 1
+        for i in range(1, len(prices)):
+            if prices[i] < buy:
+                buy = prices[i] # find the lowest buy
+            elif prices[i] - buy > profit:
+                profit = prices[i] - buy
         
-        return maxProfit
+        return profit
+
+        # # Sol 1
+        # left = 0 
+        # right = left + 1
+        # maxProfit = 0
+
+        # while right < len(prices):
+        #     if prices[left] < prices[right]:
+        #         maxProfit = max(maxProfit, prices[right] - prices[left])
+        #     else:
+        #         left = right # skip to the next lowest
+
+        #     right += 1
+        
+        # return maxProfit
             
