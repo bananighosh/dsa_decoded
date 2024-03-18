334c9ed2e05cd805d8638995658363473286c69b
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-03-18 17:14:01 -0400

    Time: 123 ms (25.59%) | Memory: 16.7 MB (50.74%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.py
@@ -1,6 +1,9 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
 
+
+
+
         freq = defaultdict(int)
         left = 0
         maxLen = 0
@@ -8,6 +11,9 @@ class Solution:
         for right in range(len(s)):
             freq[s[right]] += 1
 
+            # Intuition behind this is  the length of the sliding window minus 
+            # the most frequent element/char in the the sliding window
+            # indicates the number of elements that are mismatched
             while (right - left + 1) - max(freq.values()) > k:
                 freq[s[left]] -= 1
                 left += 1
