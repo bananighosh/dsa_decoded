f358e377cbfffacb24bb5ac78d948cc3e9a0f6fd
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-03-19 08:27:55 -0400

    Time: 43 ms (23.66%) | Memory: 16.6 MB (12.96%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -6,40 +6,43 @@
 class Solution:
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         ## Sol 1 : iterative
-        head = ListNode()
-        tail = head
+        # head = ListNode()
+        # tail = head
         
-        while list1 and list2:
-            if list1.val < list2.val:
-                tail.next = list1
-                list1 = list1.next
-            else:
-                tail.next = list2
-                list2 = list2.next
+        # while list1 and list2:
+        #     if list1.val < list2.val:
+        #         tail.next = list1
+        #         list1 = list1.next
+        #     else:
+        #         tail.next = list2
+        #         list2 = list2.next
             
-            tail = tail.next
+        #     tail = tail.next
         
-        if list1:
-            tail.next = list1
-        elif list2:
-            tail.next = list2
+        # if list1:
+        #     tail.next = list1
+        # elif list2:
+        #     tail.next = list2
         
-        return head.next
+        # return head.next
 
         ## Sol2 : recursive
-        # def mergeRecusive(list1, list2):
-        #     if list1 and list2:
-        #         if list1.val < list2.val:
-        #             list1.next = mergeRecusive(list1.next, list2)
-        #             return list1
-        #         else:
-        #             list2.next = mergeRecusive(list1, list2.next)
-        #             return list2
-            
-        #     if list1:
-        #         return list1
-        #     else:
-        #         return list2
 
-        # return mergeRecursive(list1, list2)
+        if not list1 or not list2:
+            return list1 or list2
+        
+        
+        if list1.val <= list2.val:
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
+        
+        # handled in the 1st statement in python
+        # if list1:
+        #     return list1
+        # else:
+        #     return list2
+
                 
