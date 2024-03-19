87c6c8fa903b8f938df07a3689aaa7236387fae4
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-03-19 08:38:11 -0400

    Time: 48 ms (40.13%) | Memory: 19 MB (73.38%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -7,15 +7,32 @@
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
 
-        visited = set()
-        curr = head
+        # Sol 2: using fast and slow pointer technique
+        # intuition if at any point fast and slow pointer are equal loop exists
 
-        while curr:
-            if curr in visited:
-                return True
+        fast = slow = head
+
+        while fast and fast.next:
             
-            visited.add(curr)
-            curr = curr.next
+            fast = fast.next.next
+            slow = slow.next
+
+            if fast == slow:
+                return True
         
         return False
+
+        # Sol1 : using hashset - O(N) O(N)
+        # visited = set()
+        # curr = head
+
+        # # instead of just the value add the entire node for the list
+        # while curr:
+        #     if curr in visited:
+        #         return True
+            
+        #     visited.add(curr)
+        #     curr = curr.next
+        
+        # return False
         
