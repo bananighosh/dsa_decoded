1d55f1357528d6b50d3942558523b0dc20f68039
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-03-20 15:32:59 -0400

    Time: 33 ms (85.52%) | Memory: 17.4 MB (32.70%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.py
@@ -0,0 +1,32 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+"""
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+
+        # intuition: traverse the linkedlist an store in a hasmap
+        # traverse the hashmap and create the output linkedlist
+
+        curr = head
+        random_map = {None : None}
+
+
+        while curr:
+            random_map[curr] = Node(curr.val)
+            curr = curr.next
+        
+        curr = head
+        while curr:
+            new = random_map[curr]
+            new.next = random_map[curr.next]
+            new.random = random_map[curr.random]
+            curr = curr.next
+        
+        return random_map[head]
+        
