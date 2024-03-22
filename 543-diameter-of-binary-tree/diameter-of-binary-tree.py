25bff5d4d226e009409201a1c08467c9e5d7f971
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-03-22 14:46:30 -0400

    Time: 39 ms (79.70%) | Memory: 19.2 MB (16.15%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -6,6 +6,24 @@
 #         self.right = right
 class Solution:
     def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        #Sol 3        
+        dia = [0]
+        
+        def dfs(root):
+            if not root:
+                return 0
+            
+            left = dfs(root.left)
+            right = dfs(root.right)
+
+            dia[0] = max(dia[0], left + right)
+
+            return 1 + max(left, right)
+        
+        dfs(root)
+
+        return dia[0]
+
         # Sol 2
         # if not root:
         #     return 0
@@ -28,21 +46,21 @@ class Solution:
 
         # return max((left_height + right_height), max(left_dia, right_dia))
 
-        # Sol 1
-        if not root:
-            return 0
+        # # Sol 1
+        # if not root:
+        #     return 0
 
-        dia = [0] # using it as global variable
+        # dia = [0] # using it as global variable
 
-        def dfs(root):
-            if not root:
-                return -1
+        # def dfs(root):
+        #     if not root:
+        #         return -1
             
-            left = dfs(root.left)
-            right = dfs(root.right)
-            dia[0] = max(dia[0], left + right + 2)
+        #     left = dfs(root.left)
+        #     right = dfs(root.right)
+        #     dia[0] = max(dia[0], left + right + 2)
 
-            return 1 + max(left, right)
+        #     return 1 + max(left, right)
         
-        dfs(root)
-        return dia[0]
+        # dfs(root)
+        # return dia[0]
