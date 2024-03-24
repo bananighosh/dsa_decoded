87aa3f8c3063d11f8080b33ea61a047e77ac8ed5
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-03-24 15:17:46 -0400

    Time: 44 ms (63.55%) | Memory: 19.4 MB (67.14%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -6,24 +6,47 @@
 #         self.right = right
 class Solution:
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
-        
+        #Sol 1 : O(n)
         # traverse left of the BST for finding the lower values than the parent
         # keep decrementing k
         # once the depth is reached that's the smallest value
         # in recursion when k = 0, we have reached the kth smallest
+        count = [0]
+        res = [0]
 
-        min_heap = []
-
-        def inorder_dfs(root):
-            if not root:
+        def inorder_dfs(root, k):
+            if not root or count[0] >= k:
                 return
+            
+            inorder_dfs(root.left, k)
 
-            inorder_dfs(root.left)
-            heapq.heappush(min_heap, root.val)
-            inorder_dfs(root.right)
+            count[0] += 1
+            if count[0] == k:
+                res[0] = root.val
+                return
+            
+            inorder_dfs(root.right, k)
         
-        inorder_dfs(root)
-        return min_heap[k-1]
+        inorder_dfs(root, k)
+        return res[0]
+            
+
+        
+
+
+        # #Sol 2: O(nlogk)
+        # min_heap = []
+
+        # def inorder_dfs(root):
+        #     if not root:
+        #         return
+
+        #     inorder_dfs(root.left)
+        #     heapq.heappush(min_heap, root.val)
+        #     inorder_dfs(root.right)
+        
+        # inorder_dfs(root)
+        # return min_heap[k-1]
             
 
 
