4c060bea2eb4defcca63220f92418a66f492f3aa
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-03-26 09:29:20 -0400

    Time: 50 ms (6.75%) | Memory: 16.7 MB (30.90%) - LeetSync

--- 78-subsets/subsets.py
+++ 78-subsets/subsets.py
@@ -6,7 +6,8 @@ class Solution:
         def recurse(i):
 
             if i >= len(nums):
-                res.append(curr.copy())
+                # res.append(curr.copy())/
+                res.append(curr[:])
                 return
             
             curr.append(nums[i])
