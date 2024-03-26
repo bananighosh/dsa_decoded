573b472fb2f7e8c9a18a2bd0e9eee9a198c8445a
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-03-26 11:01:26 -0400

    Time: 42 ms (49.49%) | Memory: 16.5 MB (96.34%) - LeetSync

--- 46-permutations/permutations.py
+++ 46-permutations/permutations.py
@@ -1,20 +1,36 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
+
+        # Sol 2:
         res = []
         curr = []
-
-        def backtrack(i):
-            if i >= len(nums):
-                res.append(nums.copy())
+        def backtrack(curr, nums):
+            if not nums:
+                res.append(curr)
                 return
             
-            for j in range(i, len(nums)):
+            for i in range(len(nums)):
+                backtrack(curr +[nums[i]], nums[:i] + nums[i+1:])
+        
+        backtrack(curr, nums)
+        return res
 
-                nums[j], nums[i] = nums[i], nums[j]
-                backtrack(i + 1)
-                nums[j], nums[i] = nums[i], nums[j]
+        # Sol 1:
+        # res = []
+        # curr = []
+
+        # def backtrack(i):
+        #     if i >= len(nums):
+        #         res.append(nums.copy())
+        #         return
+            
+        #     for j in range(i, len(nums)):
+
+        #         nums[j], nums[i] = nums[i], nums[j]
+        #         backtrack(i + 1)
+        #         nums[j], nums[i] = nums[i], nums[j]
                 
         
-        backtrack(0)
-        return res
+        # backtrack(0)
+        # return res
         
