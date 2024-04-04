b745cc6277a40159fd603ea5b64f542b59241827
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-04-04 16:49:01 -0400

    Time: 541 ms (98.71%) | Memory: 23 MB (41.98%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -1,25 +1,32 @@
 class Solution:
     def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
-
+        # sol1
         # heap consists of the distance from origin and the point list, 
         # sorted on the basis of distance
 
-        minHeap = []
-        res = []
+        # minHeap = []
+        # res = []
 
-        for curr in points:
+        # for curr in points:
 
-            dist = curr[0] ** 2 + curr[1] ** 2
-            minHeap.append([dist, curr])
+        #     dist = curr[0] ** 2 + curr[1] ** 2
+        #     minHeap.append([dist, curr])
         
-        print(minHeap)
-        heapq.heapify(minHeap)
-        print(minHeap)
+        # print(minHeap)
+        # heapq.heapify(minHeap)
+        # print(minHeap)
 
-        while k != 0:
-            dist, point = heapq.heappop(minHeap)
-            res.append(point)
-            k -= 1
+        # while k != 0:
+        #     dist, point = heapq.heappop(minHeap)
+        #     res.append(point)
+        #     k -= 1
         
-        return res
-         
+        # return res 
+
+
+        #sol2
+        if k == len(points):
+            return points
+
+        points = sorted(points, key = lambda x: x[0] ** 2 + x[1] ** 2)
+        return points[:k]
