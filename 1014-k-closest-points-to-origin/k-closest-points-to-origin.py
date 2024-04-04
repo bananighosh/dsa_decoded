1f3df00f973819849044a5c6d6ea99fd69b9b9cf
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-04-04 17:00:33 -0400

    Time: 595 ms (60.36%) | Memory: 23.3 MB (13.37%) - LeetSync

--- 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
+++ 1014-k-closest-points-to-origin/k-closest-points-to-origin.py
@@ -24,9 +24,26 @@ class Solution:
         # return res 
 
 
-        #sol2
-        if k == len(points):
-            return points
+        # #sol2 - fastest
+        # if k == len(points):
+        #     return points
 
-        points = sorted(points, key = lambda x: x[0] ** 2 + x[1] ** 2)
-        return points[:k]
+        # points = sorted(points, key = lambda x: x[0] ** 2 + x[1] ** 2)
+        # return points[:k]
+
+        #sol3 maxHeap
+        maxHeap = []
+
+        size = 0
+
+        for curr in points:
+            dist = curr[0] ** 2 + curr[1] ** 2
+            heapq.heappush(maxHeap, (-1*dist, curr))
+            size += 1
+
+            if size > k:
+                heapq.heappop(maxHeap)
+                size -= 1
+            
+        res = [i[1] for i in maxHeap]
+        return res
