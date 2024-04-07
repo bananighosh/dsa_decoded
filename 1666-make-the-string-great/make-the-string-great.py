c6f287a588537dd2a8ecfdc43c80d60d323fc8de
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-04-06 21:50:41 -0400

    Time: 35 ms (76.12%) | Memory: 16.5 MB (42.12%) - LeetSync

--- 1666-make-the-string-great/make-the-string-great.py
+++ 1666-make-the-string-great/make-the-string-great.py
@@ -1,18 +1,30 @@
 class Solution:
     def makeGood(self, s: str) -> str:
 
-        left = 0
-        char_arr = list(s)
+        # Sol 2: stack:
+        stack= []
 
-        for right in range(len(s)):
-
-            if left > 0 and abs(ord(char_arr[right]) -  ord(char_arr[left - 1])) == 32:
-                left -= 1
+        for c in s:
+            if stack and abs(ord(stack[-1]) - ord(c)) == 32:
+                stack.pop()
             else:
-                char_arr[left] = char_arr[right]
-                left += 1
+                stack.append(c)
         
-        return ''.join(char_arr[:left])
+        return ''.join(stack)
+
+        # # Sol 1: O(n) O(1)
+        # left = 0
+        # char_arr = list(s)
+
+        # for right in range(len(s)):
+
+        #     if left > 0 and abs(ord(char_arr[right]) -  ord(char_arr[left - 1])) == 32:
+        #         left -= 1
+        #     else:
+        #         char_arr[left] = char_arr[right]
+        #         left += 1
+        
+        # return ''.join(char_arr[:left])
                 
                 
         
