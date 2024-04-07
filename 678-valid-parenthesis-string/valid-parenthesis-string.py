73a002da58f9a905abfb07f59c745c8715f78d9a
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-04-06 21:18:13 -0400

    Time: 24 ms (98.58%) | Memory: 16.4 MB (91.06%) - LeetSync

--- 678-valid-parenthesis-string/valid-parenthesis-string.py
+++ 678-valid-parenthesis-string/valid-parenthesis-string.py
@@ -1,34 +1,56 @@
 class Solution:
     def checkValidString(self, s: str) -> bool:
 
-        #Sol 2 : instead of the chars add the indices to the stacks
+        # Sol 3 : optimised O(n) O(1)
+        # leftmin makes all "asterisk" to ")", leftmax makes all "asterisk" to "("
+        leftmin = leftmax = 0
 
-        stack = []
-        ast = []
-
-        for i, c in enumerate(s):
-            if c == '(':
-                stack.append(i)
-            elif c == '*':
-                ast.append(i)
-            else: # c == ')'
-                if stack:
-                    stack.pop()
-                elif ast:
-                    ast.pop()
-                else:
-                    # couldn't find an open parenthesis or 
-                    # an asterisk to be treated as an open parenthesis
-                    return False
-        
-        while stack and ast:
-            if stack[-1] > ast[-1]: # if open parenthesis is not in the left of asterisk
+        for c in s:
+            if c == "(":
+                leftmax += 1
+                leftmin += 1
+            elif c == ")":
+                leftmax -= 1
+                leftmin = max(0, leftmin - 1)
+            else: #c == "*"
+                leftmax += 1
+                leftmin = max(0, leftmin - 1)
+            
+            if leftmax < 0:
                 return False
             
-            stack.pop()
-            ast.pop()
+        # implies no open brackets
+        if leftmin == 0:
+            return True
+            
+
+
+        #Sol 2 : instead of the chars add the indices to the stacks
+        # stack = []
+        # ast = []
+
+        # for i, c in enumerate(s):
+        #     if c == '(':
+        #         stack.append(i)
+        #     elif c == '*':
+        #         ast.append(i)
+        #     else: # c == ')'
+        #         if stack:
+        #             stack.pop()
+        #         elif ast:
+        #             ast.pop()
+        #         else:
+        #             # couldn't find an open parenthesis or 
+        #             # an asterisk to be treated as an open parenthesis
+        #             return False
         
-        return not stack
+        # while stack and ast:
+        #     if stack[-1] > ast[-1]: # if open parenthesis is not in the left of asterisk
+        #         return False
+        #     stack.pop()
+        #     ast.pop()
+        
+        # return not stack
 
 
 
