840ca9f916841d825be804374b3d1a7fb3793152
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-05-27 00:39:26 -0700

    Time: 49 ms (20.04%) | Memory: 16.6 MB (14.52%) - LeetSync

--- 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
+++ 1730-special-array-with-x-elements-greater-than-or-equal-x/special-array-with-x-elements-greater-than-or-equal-x.py
@@ -1,15 +1,27 @@
 class Solution:
     def specialArray(self, nums: List[int]) -> int:
+        ## Sol 2:
+        nums.sort()
+        count = 0
 
-        count = Counter(nums)
-        print(count)
+        for x in range(len(nums) + 1):
+            count = sum(1 for num in nums if num >= x)
+            print(count, x)
 
-        count_so_far = 0
-
-        for i in range(max(nums), -1, -1):
-            count_so_far += count[i]
-
-            if count_so_far == i:
-                return i
-        
+            if count == x:
+                return x
         return -1
+
+        ## Sol 1: 
+        # count = Counter(nums)
+        # print(count)
+
+        # count_so_far = 0
+
+        # for i in range(max(nums), -1, -1):
+        #     count_so_far += count[i]
+
+        #     if count_so_far == i:
+        #         return i
+        
+        # return -1
