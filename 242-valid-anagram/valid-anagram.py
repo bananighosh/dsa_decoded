d037869891e95ef56d320b7a4ec7769e50d1fa68
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-09-03 23:42:12 -0400

    Time: 40 ms (91.57%) | Memory: 17.1 MB (28.18%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,23 +1,4 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
-        hmap = {}
-
-        for c in s:
-            if c not in hmap:
-                hmap[c] = 0
-            
-            hmap[c] += 1
-
-        for c in t:
-            if c not in hmap or hmap[c] < 0:
-                return False
-            else:
-                hmap[c] -= 1
-                
-        
-        for key, val in hmap.items():
-            if val != 0:
-                return False
-        return True
-        
+        return Counter(s) == Counter(t)
