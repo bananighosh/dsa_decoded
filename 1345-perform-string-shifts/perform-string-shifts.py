dd861ae5d0b830ec35aaccf1b723759659b4f8ac
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-09-13 22:26:12 -0400

    Time: 32 ms (84.82%) | Memory: 16.6 MB (76.30%) - LeetSync

--- 1345-perform-string-shifts/perform-string-shifts.py
+++ 1345-perform-string-shifts/perform-string-shifts.py
@@ -1,11 +1,11 @@
 class Solution:
     def stringShift(self, s: str, shift: List[List[int]]) -> str:
         
-        for direction, num_shifts in shift:
-            # direction = item[0]
-            # num_shifts = item[1]
+        for item in shift:
+            direction = item[0]
+            num_shifts = item[1]
             num_shifts %= len(s)
-
+            
             if( direction == 0 ):
                 s = s[num_shifts:] + s[:num_shifts]
             else:
