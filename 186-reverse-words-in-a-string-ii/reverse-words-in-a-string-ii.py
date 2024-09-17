bb33f9b1e5b35affcb85eb55fbc77accb0dc937d
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-09-17 13:21:09 -0400

    Time: 189 ms (33.92%) | Memory: 20.9 MB (8.01%) - LeetSync

--- 186-reverse-words-in-a-string-ii/reverse-words-in-a-string-ii.py
+++ 186-reverse-words-in-a-string-ii/reverse-words-in-a-string-ii.py
@@ -3,24 +3,28 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        start, end = 0, len(s)-1
-        while start <= end:
-            s[start], s[end] = s[end], s[start]
-            start += 1
-            end -= 1
-        start = 0
-        for i in range(len(s)):
-            if s[i] == " ":
-                front, back = start, i-1
-                while front <= back:
-                    s[front], s[back] = s[back], s[front]
-                    front += 1
-                    back -= 1
-                start = i+1
-        front, back = start, len(s)-1
-        while front <= back:
-            s[front], s[back] = s[back], s[front]
-            front += 1
-            back -= 1
+
+        # reverse each word and then reverse the entire string
+        # reverse by swapping high low ends
+
+        def reverse(s, low, high):
+            while low < high:
+                temp = s[low]
+                s[low] = s[high]
+                s[high] = temp
+                low += 1
+                high -= 1
+            return s
         
+        s.reverse() # reverse entire string
         
+        l = h = 0
+        while(l < len(s)):
+            while h < len(s) and s[h] != " ":
+                h += 1
+            
+            # h = i - 1
+
+            s = reverse(s, l, h - 1)
+            l = h + 1
+            h += 1  
