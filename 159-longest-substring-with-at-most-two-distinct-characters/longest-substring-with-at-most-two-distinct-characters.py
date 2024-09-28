68135f05b42ea2e91733b53638ed08a601cf7f74
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-09-28 15:55:15 -0400

    Time: 232 ms (72.41%) | Memory: 17.2 MB (17.49%) - LeetSync

--- 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.py
+++ 159-longest-substring-with-at-most-two-distinct-characters/longest-substring-with-at-most-two-distinct-characters.py
@@ -1,19 +1,26 @@
 class Solution:
     def lengthOfLongestSubstringTwoDistinct(self, s: str) -> int:
-        start = 0
-        hashMap = {}
-        ans = 0
-        for i, val in enumerate(s):
-            if val not in hashMap:
-                hashMap[val] = 0
-            hashMap[val] += 1
-            while len(hashMap) > 2:
-                hashMap[s[start]] -= 1
-                if hashMap[s[start]] == 0:
-                    del hashMap[s[start]]
-                start += 1
-            ans = max(ans, i-start+1)
+        n = len(s)
+        if( n < 3):
+            return n
+        left = right = 0
 
-        return ans
+        char_map = defaultdict()
+        max_len = 0
+
+        while right < n:
+            char_map[s[right]] = right
+            right += 1
+                
+            if len(char_map) == 3:
+                char_to_remove_idx = min(char_map.values())
+                # char_set.discard(s[char_to_remove_idx])
+                del char_map[s[char_to_remove_idx]]
+                left = char_to_remove_idx + 1
+                # right += 1
+            
+            max_len = max(max_len, right - left)
+        
+        return max_len
 
         
