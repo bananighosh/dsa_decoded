6525ba7acfd93567cd35c06195bd682999a1ad16
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-10-01 13:26:18 -0400

    Time: 415 ms (99.59%) | Memory: 30 MB (19.54%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.py
@@ -1,22 +1,17 @@
-def findKthLargest(nums, k):
-    min_value = min(nums)
-    max_value = max(nums)
-    count = [0] * (max_value - min_value + 1)
+class Solution:
+    def findKthLargest(self, nums, k):
+        min_value = min(nums)
+        max_value = max(nums)
+        count = [0] * (max_value - min_value + 1)
 
-    for num in nums:
-        count[num - min_value] += 1
-        
-    remain = k
-    for num in range(len(count) -1, -1, -1):
-        remain -= count[num]
-        if remain <= 0:
-            return num + min_value
+        for num in nums:
+            count[num - min_value] += 1
+            
+        remain = k
+        for num in range(len(count) -1, -1, -1):
+            remain -= count[num]
+            if remain <= 0:
+                return num + min_value
 
-    return -1
-with open('user.out', 'w') as f:
-    inputs = map(loads, stdin)
-    for nums in inputs:
-        k = next(inputs)
-        print(findKthLargest(nums, k), file=f)
-exit()
+        return -1
         
