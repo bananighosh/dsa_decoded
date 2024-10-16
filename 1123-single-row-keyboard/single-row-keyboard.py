333f4a3b744e731036c01238c6aff52e2e71ad0c
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-10-15 22:01:28 -0400

    Time: 40 ms (80.39%) | Memory: 16.7 MB (32.54%) - LeetSync

--- 1123-single-row-keyboard/single-row-keyboard.py
+++ 1123-single-row-keyboard/single-row-keyboard.py
@@ -1,10 +1,12 @@
 class Solution:
     def calculateTime(self, keyboard: str, word: str) -> int:
 
-        indexMap = defaultdict(int)
+        # indexMap = defaultdict(int)
 
-        for idx, c in enumerate(keyboard):
-            indexMap[c] = idx
+        # for idx, c in enumerate(keyboard):
+        #     indexMap[c] = idx
+
+        indexMap = {char: idx for idx, char in enumerate(keyboard)}
 
         time = 0
         prev_idx = 0
