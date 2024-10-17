26dfcd33834e50678140435b236701d00922acef
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-10-16 20:57:41 -0400

    Time: 683 ms (21.26%) | Memory: 25.2 MB (5.94%) - LeetSync

--- 127-word-ladder/word-ladder.py
+++ 127-word-ladder/word-ladder.py
@@ -1,34 +1,31 @@
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
         adjDict = defaultdict(set)
-        hashSet = set(wordList)
         letters = "abcdefghijklmnopqrstuvwxyz"
+        hashSet = set(wordList)
         for word in [beginWord, *wordList]:
             for i in range(len(word)):
                 for letter in letters:
-                    nWord = word[:i] + letter + word[i+1:]
-                    if nWord in hashSet:
-                        adjDict[word].add(nWord)
-                        adjDict[nWord].add(word) 
-
+                    nword = word[:i] + letter + word[i+1:]
+                    if nword in hashSet:
+                        adjDict[word].add(nword)
+        
         vis = set()
         st = [beginWord]
         dist = 0
         while st:
-            curr = []
             dist += 1
+            temp = []
             for word in st:
-                if word == endWord:
-                    return dist
                 if word in vis:
                     continue
                 vis.add(word)
-                for words in adjDict[word]:
-                    if words in vis:
-                        continue
-                    curr.append(words)
-            st = curr
+                if word == endWord:
+                    return dist
+                for nextWord in adjDict[word]:
+                    temp.append(nextWord)
+            st = temp
         return 0
-
+            
 
                 
