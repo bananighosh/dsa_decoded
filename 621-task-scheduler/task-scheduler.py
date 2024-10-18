67c4feab48d4f2ba248e846b1bcbeaf8925d564d
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-10-17 22:29:00 -0400

    Time: 151 ms (100.00%) | Memory: 18.4 MB (5.62%) - LeetSync

---
+++ 621-task-scheduler/task-scheduler.py
@@ -0,0 +1,24 @@
+class Solution:
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        maxHeap = [-i for i in Counter(tasks).values()]
+        heapq.heapify(maxHeap)
+        q = deque()
+
+        time = 0
+        while maxHeap or q:
+            time += 1
+            if maxHeap:
+                cnt = 1 + heapq.heappop(maxHeap)
+                if cnt:
+                    q.append((cnt, time + n))
+            if q and time == q[0][1]:
+                cnt, time  = q.popleft()
+                heapq.heappush(maxHeap,cnt)
+        return time
+
+
+
+
+        
+
+        
