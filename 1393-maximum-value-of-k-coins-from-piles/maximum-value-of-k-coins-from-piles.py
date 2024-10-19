fc1ef7dd38fbb613406fac049d681659f88022a4
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-10-18 22:33:06 -0400

    Time: 4060 ms (53.06%) | Memory: 323.9 MB (32.99%) - LeetSync

--- 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.py
+++ 1393-maximum-value-of-k-coins-from-piles/maximum-value-of-k-coins-from-piles.py
@@ -5,12 +5,12 @@ class Solution:
         def dfs(i, coins):
             if i == n:
                 return 0
-            res = dfs(i+1, coins)
+            res = 0
             curPile = 0
             for j in range(min(coins, len(piles[i]))):
                 curPile += piles[i][j]
                 res = max(res, curPile + dfs(i+1, coins-j-1))
+            res = max(res, dfs(i+1, coins))
             return res
-        
         return dfs(0, k)
         
