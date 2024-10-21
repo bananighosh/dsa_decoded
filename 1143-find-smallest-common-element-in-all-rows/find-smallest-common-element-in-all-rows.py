c48ac079191f0bce9e1a8091e3091bf9b5bbe7ad
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-10-20 23:26:55 -0400

    Time: 12 ms (99.43%) | Memory: 49.2 MB (5.43%) - LeetSync

--- 1143-find-smallest-common-element-in-all-rows/find-smallest-common-element-in-all-rows.py
+++ 1143-find-smallest-common-element-in-all-rows/find-smallest-common-element-in-all-rows.py
@@ -1,13 +1,29 @@
 class Solution:
     def smallestCommonElement(self, mat: List[List[int]]) -> int:
-        hashMap = [set(row) for row in mat ]
-        for num in mat[0]:
-            flag = True
-            for row in hashMap:
-                if num not in row:
-                    flag = False
+        smallestCommEle = mat[0][0]
+        commSet = [set(row) for row in mat]
+
+        for n in mat[0]:
+            found = True
+            for row in commSet:
+                if n not in row:
+                    found = False
                     break
-            if flag:
-                return num
-        return -1
+            if found:
+                return n
         
+        return -1
+
+        # commMap = defaultdict()
+        # commSet = mat[0]
+        # print(commSet)
+
+        # for i in range(1,len(mat)):
+        #     currSet = mat[i]
+        #     print(currSet)
+        #     for n in currSet:
+        #         if n in commSet:
+        #             print(n)
+        #             smallestCommEle = min(smallestCommEle,  n)
+        
+        # return smallestCommEle
