493f4555c6fdb8dd13b63cdd97f322be760d9adb
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-10-24 17:17:36 -0400

    Time: 60 ms (96.01%) | Memory: 16.8 MB (62.82%) - LeetSync

--- 723-candy-crush/candy-crush.py
+++ 723-candy-crush/candy-crush.py
@@ -2,31 +2,64 @@ class Solution:
     def candyCrush(self, board: List[List[int]]) -> List[List[int]]:
         m, n = len(board), len(board[0])
 
-        def find():
-            crushed_set = set()
+        # def find():
+        #     crushed_set = set()
+        #     for r in range(1, m - 1):
+        #         for c in range(n):
+        #             if board[r][c] == 0:
+        #                 continue
+        #             if board[r][c] == board[r+1][c] == board[r-1][c]:
+        #                 crushed_set.add((r , c))
+        #                 crushed_set.add((r + 1, c))
+        #                 crushed_set.add((r - 1, c))
+
+        #     for r in range(m):
+        #         for c in range(1, n - 1 ):
+        #             if board[r][c] == 0:
+        #                 continue
+        #             if board[r][c] == board[r][c + 1] == board[r][c - 1]:
+        #                 crushed_set.add((r , c))
+        #                 crushed_set.add((r, c + 1))
+        #                 crushed_set.add((r, c - 1))
+        #     return crushed_set
+            
+        # def crush(crushed_set):
+        #     for (r,c) in crushed_set:
+        #         board[r][c] = 0
+        
+        def find_and_crush():
+            complete = True
+
             for r in range(1, m - 1):
                 for c in range(n):
                     if board[r][c] == 0:
                         continue
-                    if board[r][c] == board[r+1][c] == board[r-1][c]:
-                        crushed_set.add((r , c))
-                        crushed_set.add((r + 1, c))
-                        crushed_set.add((r - 1, c))
+                    if abs(board[r][c]) == abs(board[r+1][c]) == abs(board[r-1][c]):
+                        board[r][c] = -abs(board[r][c])
+                        board[r + 1][c] = -abs(board[r + 1][c])
+                        board[r - 1][c] = -abs(board[r - 1][c])
+                        complete = False
 
             for r in range(m):
                 for c in range(1, n - 1 ):
                     if board[r][c] == 0:
                         continue
-                    if board[r][c] == board[r][c + 1] == board[r][c - 1]:
-                        crushed_set.add((r , c))
-                        crushed_set.add((r, c + 1))
-                        crushed_set.add((r, c - 1))
-            return crushed_set
+                    if abs(board[r][c]) == abs(board[r][c + 1]) == abs(board[r][c - 1]):
+                        board[r][c] = -abs(board[r][c])
+                        board[r][c + 1] = -abs(board[r][c + 1])
+                        board[r][c - 1] = -abs(board[r][c - 1])
+                        complete = False
             
-        def crush(crushed_set):
-            for (r,c) in crushed_set:
-                board[r][c] = 0
-        
+            for r in range(m):
+                for c in range(n):
+                    if board[r][c] < 0:
+                        board[r][c] = 0
+
+            return complete
+            
+        # def crush(crushed_set):
+        #     for (r,c) in crushed_set:
+        #         board[r][c] = 0
         def drop():
             for c in range(n):
                 lowest_zero = -1
@@ -38,10 +71,13 @@ class Solution:
                         board[r][c], board[lowest_zero][c] = board[lowest_zero][c], board[r][c]
                         lowest_zero -= 1
         
-        crushed_set = find()
-        while crushed_set:
-            crush(crushed_set)
+        # crushed_set = find()
+        # while crushed_set:
+        #     crush(crushed_set)
+        #     drop()
+        #     crushed_set = find()
+
+        while not find_and_crush():
             drop()
-            crushed_set = find()
         
         return board
