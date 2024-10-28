c85e619e7d24a149095e0dd5d6df520d07c4edae
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-10-27 21:26:16 -0400

    Time: 150 ms (32.43%) | Memory: 34.8 MB (44.67%) - LeetSync

--- 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.py
+++ 2586-longest-square-streak-in-an-array/longest-square-streak-in-an-array.py
@@ -1,34 +1,18 @@
 class Solution:
     def longestSquareStreak(self, nums: List[int]) -> int:
-        #sort the array
+        # sort the array
         # search the array for squares of each and update count - but O(n^2)
         # O(n) or <O(n^2) ??
-
+        # sol1 : binary search
+        
+        # sol2: 
         nums.sort()
         visited = set(nums)
-
         longest_streak = 0
-
-        # def binary_search(nums, target):
-        #     left, right = 0, len(nums) - 1
-
-        #     while left <= right:
-        #         mid = (left + right) // 2
-        #         if nums[mid] == target:
-        #             return True
-        #         elif nums[mid] < target:
-        #             left = mid + 1
-        #         else:
-        #             right = mid - 1
-        #     return False
 
         print(nums)
         for num in nums:
-            # if num in visited:
-            #     continue
-            
             curr_streak = num
-            # sq_curr_streak = num * num
             curr_streak_len = 0
 
             while curr_streak in visited:
@@ -39,16 +23,3 @@ class Solution:
                 longest_streak = max(longest_streak, curr_streak_len)
             
         return longest_streak if longest_streak > 1 else -1
-
-            
-        #     while curr_streak * curr_streak <= 10**5:
-        #         if binary_search(nums, curr_streak * curr_streak):
-        #             curr_streak *= curr_streak
-        #             visited.add(curr_streak)
-        #             curr_streak_len += 1
-        #         else:
-        #             break
-            
-        #     longest_streak = max(longest_streak, curr_streak_len)
-        
-        # return longest_streak if longest_streak >= 2 else -1
