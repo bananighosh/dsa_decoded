4b4572bbcf22133520fa30dc43ebcf0aa8abb29f
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-10-29 15:45:26 -0400

    Time: 0 ms (100.00%) | Memory: 16.6 MB (74.90%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.py
@@ -1,6 +1,7 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
 
+        ## sol 1
         # @cache
         # def minCost(n):
         #     if n == 0:
@@ -11,18 +12,34 @@ class Solution:
         
         #     return cost[n] + min(minCost(n - 1), minCost(n - 2))
 
-        def minCostDP(n):
-            if dp[n] != -1:
-                return dp[n]
-            if n <= 1:
-                dp[n] = cost[n]
-            else:
-                dp[n] = cost[n] + min(minCostDP(n - 1), minCostDP(n - 2))
+        ## sol 2
+        # def minCostDP(n):
+        #     if dp[n] != -1:
+        #         return dp[n]
+        #     if n <= 1:
+        #         dp[n] = cost[n]
+        #     else:
+        #         dp[n] = cost[n] + min(minCostDP(n - 1), minCostDP(n - 2))
             
-            return dp[n]
+        #     return dp[n]
 
+        # n = len(cost)
+        # dp = [-1] * n
+        # return min(minCostDP(n - 1), minCostDP(n - 2))
+
+        ## sol 3
         n = len(cost)
-        dp = [-1] * n
-        return min(minCostDP(n - 1), minCostDP(n - 2))
 
+        if n == 1:
+            return cost[0]
+
+        prev , curr = cost[0], cost[1]
+               
+        for i in range(2, n):
+            next = cost[i] + min(prev, curr)
+            prev = curr
+            curr = next
+        
+        return min(prev, curr)
+ 
         
