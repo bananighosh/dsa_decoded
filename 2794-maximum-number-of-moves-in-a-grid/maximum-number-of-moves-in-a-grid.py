961f497ce2048bb9c6ab4abd2df813732630d60b
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-10-29 12:03:28 -0400

    Time: 182 ms (42.42%) | Memory: 42.9 MB (5.30%) - LeetSync

--- 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.py
+++ 2794-maximum-number-of-moves-in-a-grid/maximum-number-of-moves-in-a-grid.py
@@ -5,21 +5,22 @@ class Solution:
         @cache
         def dfs(i,j):
             curr = grid[i][j]
-            up, forward, down = 0,0,0
+            max_val = 0
+            # up, forward, down = 0,0,0
             if j+1 in range(cols):
                 if grid[i][j+1] > curr:
-                    forward = dfs(i, j+1)
-                if i+1 in range(rows):
-                    if grid[i+1][j+1] > curr:
-                        down = dfs(i+1, j+1)
-                if i-1 in range(rows):
-                    if grid[i-1][j+1] > curr:
-                        up = dfs(i-1, j+1)
-            return 1 + max(up, forward, down)
+                    # forward = dfs(i, j+1)
+                    max_val = max(max_val, dfs(i, j + 1))
+                if i+1 in range(rows) and grid[i+1][j+1] > curr:
+                        # down = dfs(i+1, j+1)
+                    max_val = max(max_val, dfs(i + 1, j + 1))
+                if i-1 in range(rows) and grid[i-1][j+1] > curr:
+                        # up = dfs(i-1, j+1)
+                    max_val = max(max_val, dfs(i - 1, j + 1))
+            # return 1 + max(up, forward, down)
+            return 1 + max_val
 
         ans = 0
-        
-        
         for i in range(rows):
             ans = max(ans, dfs(i,0)-1)
         return ans
