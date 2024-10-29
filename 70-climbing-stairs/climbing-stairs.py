492dda965f9bcb84569688fde1768804c8b0fc6f
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-10-29 14:11:31 -0400

    Time: 0 ms (100.00%) | Memory: 16.4 MB (74.09%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -4,10 +4,19 @@ class Solution:
         if n == 1:
             return 1
         
-        dp = [0 for _ in range(n + 1)]
-        dp[1] = 1
-        dp[2] = 2
-        for i in range(3, n + 1):
-            dp[i] = dp[i - 1] + dp[i - 2]
+        # dp = [0 for _ in range(n + 1)]
+        # dp[1] = 1
+        # dp[2] = 2
+        # for i in range(3, n + 1):
+        #     dp[i] = dp[i - 1] + dp[i - 2]
+
+        prev, curr = 1, 2
+
+        for i in range(3, n+1):
+            next = prev + curr
+            prev = curr
+            curr = next
+        
+        return curr
         
         return dp[n]
