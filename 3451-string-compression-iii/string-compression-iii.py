811a26d96e2b0fe1aa8baf5b7edd4d3c450d542a
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-11-03 21:53:37 -0500

    Time: 165 ms (85.11%) | Memory: 28.4 MB (8.56%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.py
@@ -0,0 +1,36 @@
+class Solution:
+    def compressedString(self, word: str) -> str:
+
+        res = []
+        n = len(word)
+
+        prev = word[0]
+        cnt = 1
+
+        left = 0
+
+        for right in range(1, n):
+            if word[right] == word[left]:
+                if cnt == 9:
+                    res.append(str(cnt))
+                    res.append(word[left])
+                    cnt = 0
+                cnt += 1
+
+            else:
+                res.append(str(cnt))
+                res.append(word[left])
+                left = right
+                cnt = 1
+        
+        res.append(str(cnt))
+        res.append(word[left])
+    
+        
+        return "".join(res)
+
+      
+
+
+
+        
