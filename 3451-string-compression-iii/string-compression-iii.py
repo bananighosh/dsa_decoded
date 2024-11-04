927498b0619ea7e9b4365a0b44ce8999d9dcf7a3
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-11-03 22:00:59 -0500

    Time: 151 ms (88.65%) | Memory: 18.8 MB (95.97%) - LeetSync

--- 3451-string-compression-iii/string-compression-iii.py
+++ 3451-string-compression-iii/string-compression-iii.py
@@ -1,22 +1,15 @@
 class Solution:
     def compressedString(self, word: str) -> str:
-
         ans = ""
-
         prev = word[0]
-
         count = 0
-
         for char in word:
-            if char == prev and count == 9:
-                ans += str(count) + char
-                count = 1
-            elif char == prev:
-                count += 1
-            else:
+            if (char == prev and count == 9) or char != prev:
                 ans += str(count) + prev
                 prev = char
                 count = 1
+            elif char == prev:
+                count += 1
         ans += str(count) + prev
 
         return ans
