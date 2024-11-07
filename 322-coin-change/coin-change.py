782178d2c4405767e69f877c1a10b0f8deb9abf4
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-11-07 15:09:37 -0500

    Time: 2061 ms (5.00%) | Memory: 445.7 MB (5.04%) - LeetSync

--- 322-coin-change/coin-change.py
+++ 322-coin-change/coin-change.py
@@ -1,8 +1,10 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
 
-        @cache
+        dp = {}
         def dfs(i, curAmount):
+            if (i, curAmount) in dp:
+                return dp[(i,curAmount)]
             if i == len(coins) and curAmount == amount:
                 return 0
             if curAmount > amount or i == len(coins):
@@ -12,7 +14,9 @@ class Solution:
             pick = 1+ dfs(i, curAmount + coins[i])
             npick = dfs(i+1, curAmount)
 
-            return min(npick, pick)
+            dp[(i,curAmount)] = min(npick, pick)
+
+            return dp[(i,curAmount)]
 
         ans = dfs(0, 0)
 
