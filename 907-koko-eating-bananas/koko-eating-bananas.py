aa937dc333891f71f591cf28962719a5bef512ea
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-11-15 13:10:53 -0500

    Time: 153 ms (44.79%) | Memory: 17.6 MB (95.63%) - LeetSync

--- 907-koko-eating-bananas/koko-eating-bananas.py
+++ 907-koko-eating-bananas/koko-eating-bananas.py
@@ -1,21 +1,16 @@
 class Solution:
     def minEatingSpeed(self, piles: List[int], h: int) -> int:
 
-        left , right = 1, max(piles)
-        # res = float('inf')
-        res = max(piles)
+        left, right = 1, max(piles)
+        res = right
 
         while left <= right:
-            k = ( left + right ) // 2
+            k = (left + right) // 2
             hours = 0
-            for pile in piles:
-                # time taken to finish the pile at rate k
-                hours += math.ceil(pile / k)
-                # if pile % k == 0:
-                #     hours += pile / k
-                # else:
-                #     hours += (pile // k) + 1
 
+            for p in piles:
+                hours += math.ceil(p / k)
+            
             if hours <= h:
                 res = min(res, k)
                 right = k - 1
@@ -23,5 +18,6 @@ class Solution:
                 left = k + 1
         
         return res
-                
+
+
         
