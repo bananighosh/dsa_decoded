7678a73264ef1fbb90fb010b4ec418807d8dd40e
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-11-19 16:43:45 -0500

    Time: 134 ms (41.60%) | Memory: 33.8 MB (69.80%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.py
@@ -8,20 +8,19 @@ class Solution:
         dist_window = set()
 
         while right < len(nums):
-
+            curr_sum += nums[right]
+            
             while right - left + 1 > k or nums[right] in dist_window:
                 curr_sum -= nums[left]
                 dist_window.remove(nums[left])
                 left += 1
-            
-            curr_sum += nums[right]
-            dist_window.add(nums[right])
 
             if right - left + 1 == k:
                 max_sum = max(max_sum, curr_sum)
                 start_idx = left
                 end_idx = right
             
+            dist_window.add(nums[right])
             right += 1
         
         return max_sum
