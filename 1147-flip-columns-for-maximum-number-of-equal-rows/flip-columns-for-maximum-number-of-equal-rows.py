3419ce355379b95dfb983de1fdd6bf2a294d9df6
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-11-22 12:04:30 -0500

    Time: 64 ms (53.70%) | Memory: 20.3 MB (14.57%) - LeetSync

---
+++ 1147-flip-columns-for-maximum-number-of-equal-rows/flip-columns-for-maximum-number-of-equal-rows.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxEqualRowsAfterFlips(self, matrix: List[List[int]]) -> int:
+        # if the 1st ele is 0, we keep the row as is
+        # if the 1st ele is 1, we will take the complement of the row and add to the freq map
+
+        freq_map = Counter()
+
+        for row in matrix:
+            curr_ele = tuple(ele if row[0] == 0 else 1 - ele for ele in row)
+
+            if curr_ele not in freq_map:
+                freq_map[curr_ele] = 0
+            
+            freq_map[curr_ele] += 1
+
+        return max(freq_map.values()) 
+        
