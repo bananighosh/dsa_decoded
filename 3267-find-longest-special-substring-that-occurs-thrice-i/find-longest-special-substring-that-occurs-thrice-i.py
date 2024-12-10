70f8b1eb2dfed505a6c563dd40e8bc8da644bc6d
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-12-10 15:03:37 -0500

    Time: 112 ms (13.87%) | Memory: 17.4 MB (6.29%) - LeetSync

---
+++ 3267-find-longest-special-substring-that-occurs-thrice-i/find-longest-special-substring-that-occurs-thrice-i.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maximumLength(self, s: str) -> int:
+        subarrays = []
+
+        for i in range(len(s)):
+            index = i
+            while index < len(s) and s[index] == s[i]:
+                subarrays.append(s[i:index+1])
+                index += 1
+            
+            freq_map = Counter(subarrays)
+            max_len = 0
+
+        for key,value in freq_map.items():
+            if value >= 3:
+                max_len = max(max_len, len(key))
+        
+        if max_len == 0:
+            return -1
+        
+        return max_len
+        
