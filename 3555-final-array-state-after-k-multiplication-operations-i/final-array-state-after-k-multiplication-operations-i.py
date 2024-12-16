c0566cab4aa6ad77a08ddaee609addf1003e16be
bananighosh <144469165+bananighosh@users.noreply.github.com>
2024-12-16 17:34:53 -0500

    Time: 7 ms (13.07%) | Memory: 17.3 MB (19.20%) - LeetSync

--- 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.py
+++ 3555-final-array-state-after-k-multiplication-operations-i/final-array-state-after-k-multiplication-operations-i.py
@@ -1,11 +1,8 @@
 class Solution:
     def getFinalState(self, nums: List[int], k: int, multiplier: int) -> List[int]:
-        curr_min, curr_i = nums[0], 0
-        #heap ??
         res = nums
 
         pq = []
-
         for i, val in enumerate(nums):
             heappush(pq, (val, i))
         
@@ -15,11 +12,6 @@ class Solution:
             heappush(pq, (res[min_idx], min_idx))
 
         return res
-        # for i in range(k):
-        #     for j, val in enumerate(nums):
-        #         if curr_min == val:
-
-        #         curr_min = min(curr_min, val)
 
 
         
