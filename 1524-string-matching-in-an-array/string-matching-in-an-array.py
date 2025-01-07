70b73ddd4795ce9af8511b61237be25cb2dc5b24
bananighosh <144469165+bananighosh@users.noreply.github.com>
2025-01-07 16:58:53 -0500

    Time: 0 ms (100.00%) | Memory: 17.9 MB (15.22%) - LeetSync

---
+++ 1524-string-matching-in-an-array/string-matching-in-an-array.py
@@ -0,0 +1,10 @@
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        s = ' '.join(words)
+        res = []
+        for w in words:
+            if s.count(w) > 1:
+                res.append(w)
+        
+        return res
+        
