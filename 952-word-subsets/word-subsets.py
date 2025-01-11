49d01f38075320aa39310058b7952c9d04869435
bananighosh <144469165+bananighosh@users.noreply.github.com>
2025-01-10 19:16:33 -0500

    Time: 518 ms (29.41%) | Memory: 21.3 MB (11.76%) - LeetSync

--- 952-word-subsets/word-subsets.py
+++ 952-word-subsets/word-subsets.py
@@ -1,21 +1,10 @@
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        maxFreq = {}
-        for word in words2:
-            for c, n in Counter(word).items():
-                if c not in maxFreq or maxFreq[c] < n:
-                    maxFreq[c] = n
-        res = []
-        for word in words1:
-            universal = True
-            count = Counter(word)
-            for c, n in maxFreq.items():
-                if c not in count or n > count[c]:
-                    universal = False
-                    break
-            if universal: res.append(word)
-        
-        return res
 
+        w2_freq = Counter()
         
-        
+        for w in words2:
+            for ch,cnt in Counter(w).items():
+                w2_freq[ch] = max(w2_freq[ch], cnt)
+
+        return [s for s in words1 if Counter(s)>=w2_freq]
