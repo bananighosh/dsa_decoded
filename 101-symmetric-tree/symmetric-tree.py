e83a1117ddf7be608ad5d6404f4e53ff8954427a
bananighosh <144469165+bananighosh@users.noreply.github.com>
2025-11-23 16:49:22 -0500

    Time: 4 ms (7.02%) | Memory: 12.8 MB (20.65%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.py
@@ -0,0 +1,27 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isSymmetric(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: bool
+        """
+        # logic: if left tree is equal to right tree
+
+        def dfs(t1, t2):
+            if not t1 and not t2:
+                return True
+            
+            if not t1 or not t2:
+                return False
+            
+            return (t1.val == t2.val) and \
+                    dfs(t1.left, t2.right) and dfs(t1.right, t2.left)
+                    # reason: This is like a person looking at a mirror. The reflection in the mirror has the same head, but the reflection's right arm corresponds to the actual person's left arm, and vice versa
+
+        return dfs(root,root)
+        
