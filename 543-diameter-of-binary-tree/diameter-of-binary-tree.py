ba055b8ce9b6dba04472b4399d735ff63187955f
bananighosh <144469165+bananighosh@users.noreply.github.com>
2025-11-23 01:52:01 -0500

    Time: 11 ms (72.08%) | Memory: 26.5 MB (15.33%) - LeetSync

--- 543-diameter-of-binary-tree/diameter-of-binary-tree.py
+++ 543-diameter-of-binary-tree/diameter-of-binary-tree.py
@@ -1,66 +1,27 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
-        #Sol 3        
-        dia = [0]
-        
-        def dfs(root):
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        def dfs(root, dia):
             if not root:
                 return 0
             
-            left = dfs(root.left)
-            right = dfs(root.right)
+            lh = dfs(root.left, dia)
+            rh = dfs(root.right, dia)
 
-            dia[0] = max(dia[0], left + right)
+            dia[0] = max(dia[0], lh + rh)
 
-            return 1 + max(left, right)
+            return 1 + max(lh, rh)
         
-        dfs(root)
-
+        dia = [0]
+        dfs(root, dia)
         return dia[0]
-
-        # Sol 2
-        # if not root:
-        #     return 0
-
-        # # find the height of a binary tree
-        # def height(root):
-        #     if not root:
-        #         return 0
-            
-        #     l_height = height(root.left)
-        #     r_height = height(root.right)
-
-        #     return 1 + max(l_height, r_height)
         
-        # left_height = height(root.left)
-        # right_height = height(root.right)
-
-        # left_dia = self.diameterOfBinaryTree(root.left)
-        # right_dia = self.diameterOfBinaryTree(root.right)
-
-        # return max((left_height + right_height), max(left_dia, right_dia))
-
-        # # Sol 1
-        # if not root:
-        #     return 0
-
-        # dia = [0] # using it as global variable
-
-        # def dfs(root):
-        #     if not root:
-        #         return -1
-            
-        #     left = dfs(root.left)
-        #     right = dfs(root.right)
-        #     dia[0] = max(dia[0], left + right + 2)
-
-        #     return 1 + max(left, right)
-        
-        # dfs(root)
-        # return dia[0]
