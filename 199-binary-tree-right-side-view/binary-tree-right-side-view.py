3fa40e3e08db971f4d073811ad7bd72da5a6b439
bananighosh <144469165+bananighosh@users.noreply.github.com>
2025-11-27 14:49:54 -0500

    Time: 0 ms (100.00%) | Memory: 12.4 MB (93.09%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -1,27 +1,40 @@
 # Definition for a binary tree node.
-# class TreeNode:
+# class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution:
-    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+class Solution(object):
+    def rightSideView(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: List[int]
+        """
+
+        if not root:
+            return []
+        dq = deque([root])
+        level = 0
         res = []
-        q = deque([root])
 
-        while q:
-            rightmost = None
-            qLen = len(q)
+        while dq:
+            curr = []
+            for _ in range(len(dq)):
+                node = dq.popleft()
 
-            for i in range(qLen):
-                node = q.popleft()
-                if node:
-                    rightmost = node
-                    q.append(node.left)
-                    q.append(node.right)
+                if len(curr) == level:
+                    curr.append(node.val)
+                
+                if node.right:
+                    dq.append(node.right)
+                if node.left:
+                    dq.append(node.left)
             
-            if rightmost:
-                res.append(rightmost.val)
+            # res.0(curr)
+            res.extend(curr)
         
         return res
+
+
+
         
