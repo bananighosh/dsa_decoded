97323a548474ff6ffb52fcd14896481a33342b04
bananighosh <144469165+bananighosh@users.noreply.github.com>
2025-11-30 12:29:07 -0500

    Time: 0 ms (100.00%) | Memory: 18 MB (21.88%) - LeetSync

--- 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
+++ 314-binary-tree-vertical-order-traversal/binary-tree-vertical-order-traversal.py
@@ -7,16 +7,16 @@
 class Solution:
     def verticalOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
         h_map = defaultdict(list)
-        dq = deque([(root, 0)])
+        dq = deque([(root,0)])
 
         while dq:
             node, col = dq.popleft()
 
             if node:
                 h_map[col].append(node.val)
+                print(h_map)
 
                 dq.append((node.left, col - 1))
                 dq.append((node.right, col + 1))
 
-        return [h_map[x] for x in sorted(h_map.keys())]   
-        
+        return [h_map[k] for k in sorted(h_map.keys())]
