f4babe395e5e070e50750674403a7d120667dc04
bananighosh <144469165+bananighosh@users.noreply.github.com>
2025-12-28 14:44:10 -0800

    Time: 29 ms (9.96%) | Memory: 12.5 MB (81.91%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,4 +1,12 @@
-class Solution:
-    def isAnagram(self, s: str, t: str) -> bool:
-
-      return sorted(s) == sorted(t)
+class Solution(object):
+    def isAnagram(self, s, t):
+        """
+        :type s: str
+        :type t: str
+        :rtype: bool
+        """
+        s_hash = Counter(s)
+        t_hash = Counter(t)
+        print(s_hash, t_hash)
+        return s_hash == t_hash
+        
