d66f07163664184c026bd180ade60e8a7859eca7
bananighosh <144469165+bananighosh@users.noreply.github.com>
2025-12-28 17:52:47 -0800

    Time: 39 ms (10.24%) | Memory: 14.6 MB (38.02%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.py
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -1,33 +1,22 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        # using minheap we can solve this
-        # insert all the array nums and the frequencies in the maxheap
-        # pop top K elements
-        # TC : O(nlogk)
-
-        # counts the freq of elemensts in an array - Counter()
-        # freq = Counter(nums)
-        # # https://pythontic.com/algorithms/heapq/nlargest
-        # return heapq.nlargest(k, freq.keys(), key = freq.get)
-
-        # Solution 2: Bucket sort
-        # sorts based on the count of the elements (max count when distinct = len(arr))
-        # traverse the count arr k times from the end
-
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        # heap implementation - minheap with (-) sign for elem freq
+        heap = []
         freq = Counter(nums)
-        bucket = [[] for i in range(len(nums) + 1)]
-
-        # build the bucket based on the count of each element in the array
-        for num, count in freq.items():
-            bucket[count].append(num)
         
+        for num,count in freq.items():
+            heapq.heappush(heap, (-count, num))
+        
+        print(heap)
         res = []
-        for i in range(len(bucket) - 1, 0, -1):
-            for most_freq_list in bucket[i]:
-                res.append(most_freq_list)
-                if len(res) == k:
-                    return res
-
+        while len(res) < k:
+            curr = heapq.heappop(heap)
+            res.append(curr[1])
         
-
+        return res
         
