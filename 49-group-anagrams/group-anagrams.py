3a295107c7fcfd89d7448bd0e48d1f0c4d144295
bananighosh <144469165+bananighosh@users.noreply.github.com>
2025-12-28 17:36:37 -0800

    Time: 23 ms (58.15%) | Memory: 16.9 MB (41.04%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,26 +1,13 @@
-class Solution:
-    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        anagram = defaultdict(list)
+class Solution(object):
+    def groupAnagrams(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: List[List[str]]
+        """
 
-        ## Solution 1:
-        # for word in strs:
-        #     sorted_word = ''.join(sorted(word))
-        #     anagram[sorted_word].append(word)
-        
-        # return list(anagram.values())
+        res = defaultdict(list)
 
-        ## Solution 2:
-        # instead of sorting
-        for word in strs:
-            count = [0] * 26  #initialize the array for a-z 26 chars to 0 count
-
-            for c in word:
-                count[ord(c) - ord("a")] += 1
-
-            anagram[tuple(count)].append(word)
-        
-        return anagram.values()
-
-
-
+        for s in strs:
+            res[tuple(sorted(s))].append(s)
+        return list(res.values())
         
