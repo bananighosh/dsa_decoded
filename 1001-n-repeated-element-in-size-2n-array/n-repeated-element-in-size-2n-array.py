00fbe707a012626908684698267e33c10325de6e
bananighosh <144469165+bananighosh@users.noreply.github.com>
2026-01-01 19:33:18 -0800

    Time: 0 ms (100.00%) | Memory: 18.4 MB (98.97%) - LeetSync

--- 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
+++ 1001-n-repeated-element-in-size-2n-array/n-repeated-element-in-size-2n-array.py
@@ -6,4 +6,12 @@ class Solution:
                 return nums[i]
         
         return nums[0]
-        
+
+        # explanation:  
+        # given that the num is repeated "n" times
+        # so majority element concept will cover exactly half of the array
+        # if we pick min possible odd-length subarray of size 3
+        # one of the elements should be duplicate
+        # edge case : in case of 4 elements and duplicate element appears at
+        # the ends so if duplicate not found we return the 1st element
+        # as it is guaranteed one elem in the array is repeated so this edge works
