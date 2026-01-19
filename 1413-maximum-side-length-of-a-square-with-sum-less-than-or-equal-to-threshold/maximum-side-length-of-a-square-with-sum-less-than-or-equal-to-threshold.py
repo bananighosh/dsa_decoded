632ae6e985391dfe2da0c4ead434e93ae824dff5
bananighosh <144469165+bananighosh@users.noreply.github.com>
2026-01-19 12:05:39 -0800

    Time: 1346 ms (17.56%) | Memory: 26.4 MB (17.12%) - LeetSync

---
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
@@ -0,0 +1,45 @@
+class Solution:
+    def maxSideLength(self, mat: List[List[int]], threshold: int) -> int:
+        rows = len(mat)
+        cols = len(mat[0])
+
+        # stores sum of all elems from (0,0) to (i,j)
+        prefixSum = [[0] * cols for _ in range(rows)]
+
+        for r in range(rows):
+            for c in range(cols):
+                prefixSum[r][c] = mat[r][c] \
+                                    + (prefixSum[r - 1][c] if r > 0 else 0) \
+                                    + (prefixSum[r][c - 1] if c > 0 else 0) \
+                                    - (prefixSum[r - 1][c - 1] if r > 0 and c > 0 else 0)
+
+        def squareSum(r, c , r2, c2):
+            # ref notebook for this calculation and visualization
+            currSquareSum = prefixSum[r2][c2] \
+                        - (prefixSum[r - 1][c2] if r > 0 else 0) \
+                        - (prefixSum[r2][c - 1] if c > 0 else 0) \
+                        + (prefixSum[r - 1][c - 1] if r > 0 and c > 0 else 0)
+            
+            return currSquareSum
+        
+        maxSide = 0
+        # not float('-inf') because ques expects 0 len if no such square found, ref TC
+
+        for r in range(rows):
+            for c in range(cols):
+                # k is an offset to reach the diagonal end corner representing the square
+                #  (i, j) to (i + offset, j + offset)
+                for k in range(0, min(rows - r, cols - c)): 
+                    r2 = r + k
+                    c2 = c + k
+
+                    currSquareSum = squareSum(r, c, r2, c2)
+
+                    if currSquareSum <= threshold:
+                        maxSide = max(maxSide, k + 1)
+                    else:
+                        break
+        
+        return maxSide
+
+        
