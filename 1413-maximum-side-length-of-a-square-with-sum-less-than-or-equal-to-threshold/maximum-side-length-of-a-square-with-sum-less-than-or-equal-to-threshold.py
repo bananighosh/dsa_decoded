b7f493e67668ea1fe56d11d9dd4769e872bf7dc5
bananighosh <144469165+bananighosh@users.noreply.github.com>
2026-01-19 12:07:18 -0800

    Time: 1431 ms (17.56%) | Memory: 26.4 MB (22.97%) - LeetSync

--- 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
+++ 1413-maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold/maximum-side-length-of-a-square-with-sum-less-than-or-equal-to-threshold.py
@@ -8,18 +8,17 @@ class Solution:
 
         for r in range(rows):
             for c in range(cols):
-                prefixSum[r][c] = mat[r][c] \
-                                    + (prefixSum[r - 1][c] if r > 0 else 0) \
-                                    + (prefixSum[r][c - 1] if c > 0 else 0) \
-                                    - (prefixSum[r - 1][c - 1] if r > 0 and c > 0 else 0)
+                up = prefixSum[r - 1][c] if r > 0 else 0
+                left = prefixSum[r][c - 1] if c > 0 else 0
+                diag = prefixSum[r - 1][c - 1] if r > 0 and c > 0 else 0
+                prefixSum[r][c] = mat[r][c] + up + left - diag
 
         def squareSum(r, c , r2, c2):
             # ref notebook for this calculation and visualization
-            currSquareSum = prefixSum[r2][c2] \
-                        - (prefixSum[r - 1][c2] if r > 0 else 0) \
-                        - (prefixSum[r2][c - 1] if c > 0 else 0) \
-                        + (prefixSum[r - 1][c - 1] if r > 0 and c > 0 else 0)
-            
+            up = prefixSum[r - 1][c2] if r > 0 else 0
+            left = prefixSum[r2][c - 1] if c > 0 else 0
+            diag = prefixSum[r - 1][c - 1] if r > 0 and c > 0 else 0
+            currSquareSum = prefixSum[r2][c2] - up - left + diag 
             return currSquareSum
         
         maxSide = 0
