26b2f402b79d94d1bf5b6c9da89cd82f7f45bfc4
Banani Ghosh <144469165+bananighosh@users.noreply.github.com>
2026-07-18 12:25:11 -0700

    Time: 1394 ms (11.88%), Space: 22.2 MB (54.02%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -3,30 +3,21 @@ class Solution:
 
         n = len(nums)
         nums.sort()
-        res = []
+        res = set()
         for i, num in enumerate(nums):
-            if i > 0 and nums[i] == nums[i - 1]:
-                continue
-
             low, high = i + 1, n - 1
             while low < high:
                 currSum = num + nums[low] + nums[high]
                 if currSum == 0:
-                    res.append([num, nums[low], nums[high]])
+                    res.add((num, nums[low], nums[high]))
                     low += 1
-                    high -= 1 
-
-                    while low < high and nums[low] == nums[low - 1]:
-                        low += 1
-                    
-                    while low < high and nums[high] == nums[high + 1]:
-                        high -= 1
+                    high -= 1
 
                 elif currSum < 0:
                     low += 1
                 else:
                     high -= 1
         
-        return res
+        return list(res)
 
         
