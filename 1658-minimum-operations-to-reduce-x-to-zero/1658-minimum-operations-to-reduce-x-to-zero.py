967a7f74053e2952ca485e52b8c1c1463a421421
Banani Ghosh <144469165+bananighosh@users.noreply.github.com>
2026-07-18 10:15:07 -0700

    Time: 124 ms (18.74%), Space: 43.5 MB (6.7%) - LeetHub

---
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.py
@@ -0,0 +1,21 @@
+class Solution:
+    def minOperations(self, nums: List[int], x: int) -> int:
+
+        target = sum(nums) - x
+        if target == 0:
+            return len(nums)
+
+        hmap = {0:-1}
+        prefixSum = 0
+        maxLen = float("-inf")
+
+        for i, num in enumerate(nums):
+            prefixSum += num
+
+            if prefixSum - target in hmap:
+                maxLen = max(maxLen, i - hmap[prefixSum - target])
+            hmap[prefixSum] = i
+        
+        print(math.isinf(maxLen))
+        
+        return len(nums) - maxLen if not math.isinf(maxLen) else -1
