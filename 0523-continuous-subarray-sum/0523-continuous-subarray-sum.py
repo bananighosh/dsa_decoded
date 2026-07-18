7da92d7d6464f2f7fdd70be9b65ac774d5fe4651
Banani Ghosh <144469165+bananighosh@users.noreply.github.com>
2026-07-18 08:38:28 -0700

    Time: 58 ms (29.88%), Space: 39.8 MB (11.44%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.py
@@ -1,17 +1,17 @@
 class Solution:
     def checkSubarraySum(self, nums: List[int], k: int) -> bool:
-
-        prefixSumMod = 0
-        prefixSumModFreq = {0: -1}
+        prefixSum = 0
+        hmap = {0: -1}
 
         for i, num in enumerate(nums):
-            prefixSumMod = (prefixSumMod + num) % k
+            prefixSum += num
+            prefixSumMod = prefixSum % k
 
-            if prefixSumMod in prefixSumModFreq:
-                if i - prefixSumModFreq[prefixSumMod] > 1:    
+            if prefixSumMod in hmap:
+                if i - hmap[prefixSumMod] > 1:
                     return True
             else:
-                prefixSumModFreq[prefixSumMod] = i
+                hmap[prefixSumMod] = i
+        
         return False
-
         
