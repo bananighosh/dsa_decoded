9f90786cf07b0114173c0bce7668e90f92a7185f
Banani Ghosh <144469165+bananighosh@users.noreply.github.com>
2026-07-25 09:38:16 -0700

    Time: 0 ms (100%), Space: 19.3 MB (60.95%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -2,22 +2,22 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
 
         n = len(nums)
-        dp = [-1] * n
+        res = 0
+        dp = [-1] * (n + 1)
 
-        def traverse(i):
-            if i < 0: 
+        def rob(i):
+            if i < 0:
                 return 0
-            if i == 0:
-                return nums[i]
-            
             if dp[i] != -1:
                 return dp[i]
-            
-            pick = nums[i] + traverse(i - 2)
-            n_pick = traverse(i - 1)
-            dp[i] = max(pick, n_pick)
+            if i == 0:
+                dp[i] = nums[i]
 
+            pick = nums[i] + rob(i - 2) 
+            n_pick = rob(i - 1)
+
+            dp[i] = max(pick, n_pick)
             return dp[i]
         
-        return traverse(n - 1)
+        return rob(n - 1)
         
