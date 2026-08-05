5053ccd93a3cdd2c5e336281113970b1e250a71f
Banani Ghosh <144469165+bananighosh@users.noreply.github.com>
2026-08-04 23:19:20 -0700

    Time: 55 ms (99.47%), Space: 19.6 MB (85.06%) - LeetHub

--- 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.py
@@ -1,35 +1,16 @@
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
 
-        def is_valid_substring_possible(s, substr_len, k):
-            freq = {}
-            max_freq = 0
-            start = 0
+        counts = {} # char : count
+        maxCount = 0 # highest freq char
 
-            for end in range(len(s)):
-                freq[s[end]] = freq.get(s[end], 0) + 1
-                if end + 1 - start > substr_len:
-                    freq[s[start]] -= 1
-                    start += 1
-            
-                max_freq = max(max_freq, freq[s[end]])
-                if substr_len - max_freq <= k:
-                    return True
-            return False
+        l = 0
+        for r in range(len(s)):
+            counts[s[r]] = counts.get(s[r], 0) + 1
+            maxCount = max(maxCount, counts[s[r]])
 
-        lo = 1
-        hi = len(s) + 1
-
-        while lo + 1 < hi:
-            mid = lo + (hi - lo) // 2
-
-            if is_valid_substring_possible(s, mid, k):
-                lo = mid
-            else:
-                hi = mid
-        return lo
-
-
-
-
+            if r-l+1 - maxCount > k:
+                counts[s[l]] -= 1
+                l += 1
         
+        return r-l+1
