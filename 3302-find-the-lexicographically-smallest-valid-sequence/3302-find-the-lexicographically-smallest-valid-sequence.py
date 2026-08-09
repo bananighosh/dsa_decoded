50681c549fe37956941ea8e0eb22ab0d1c1a5c0d
Banani Ghosh <144469165+bananighosh@users.noreply.github.com>
2026-08-08 18:26:56 -0700

    Time: 527 ms (51.06%), Space: 47.9 MB (59.57%) - LeetHub

--- 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.py
+++ 3302-find-the-lexicographically-smallest-valid-sequence/3302-find-the-lexicographically-smallest-valid-sequence.py
@@ -1,10 +1,10 @@
 class Solution:
     def validSequence(self, word1: str, word2: str) -> List[int]:
-        m, n = len(word1), len(word2)
+        n, m = len(word1), len(word2)
 
         rightMatched = 0
-        rightMatchedCharLen = [0] * m
-        i, j = m - 1, n - 1
+        rightMatchedCharLen = [0] * n
+        i, j = n - 1, m - 1
 
         while i >= 0:
             if j >= 0 and word1[i] == word2[j]:
@@ -16,17 +16,17 @@ class Solution:
         i, j = 0, 0
         res = []
         isChangeAllowed = True
-        while i < m and j < n:
+        while i < n and j < m:
             if word1[i] == word2[j]:
                 res.append(i)
                 j += 1
             elif (
                 isChangeAllowed and 
-                i + 1 < m and 
-                rightMatchedCharLen[i + 1] >= n - j - 1):
+                i + 1 < n and 
+                rightMatchedCharLen[i + 1] >= m - j - 1):
                     res.append(i)
                     isChangeAllowed = False
                     j += 1
             i += 1
         
-        return res if j == n else []
+        return res if j == m else []
