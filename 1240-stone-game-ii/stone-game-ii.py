784e8a973b807ca24d7e847a981708246c972970
Banani Ghosh <144469165+bananighosh@users.noreply.github.com>
2026-08-09 01:41:30 -0700

    Time: 320 ms (35.31%) | Memory: 24.7 MB (61.38%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.py
+++ 1240-stone-game-ii/stone-game-ii.py
@@ -1,14 +1,32 @@
 class Solution:
     def stoneGameII(self, piles: List[int]) -> int:
-        for i in range(len(piles) - 2, -1, -1):
-            piles[i] += piles[i + 1]
+        n = len(piles)
 
-        @cache
-        def dfs(i, M):
-            if i + M * 2 >= len(piles):
-                return piles[i]
+        dp = [[[-1] * (n + 1) for _ in range(n + 1)] for _ in range(2)]
 
-            return piles[i] - min(dfs(i + j, max(M, j)) for j in range(1, M * 2 + 1))
+        def solveForAlice(person, i, M):
 
-        return dfs(0, 1)
+            if i >= n:
+                return 0
+            
+            if dp[person][i][M] != -1:
+                return dp[person][i][M]
+            
+            res = -1 if person == 1 else float("inf")
+
+            stones = 0
+
+            for x  in range(1, min(2*M, n - i)+ 1):
+
+                stones += piles[i + x -1]
+
+                if person == 1: # Alice
+                    res = max(res, stones + solveForAlice(0, i + x, max(M, x) ))
+                else:
+                    res = min(res, solveForAlice(1, i + x, max(M, x)))
+            
+            dp[person][i][M] = res
+            return res
+
+        return solveForAlice(1, 0, 1) # (person, i, M)
         
